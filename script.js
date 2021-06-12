7f287c19dbcdb67f00e345c2851d68e5a430b3b7
abhishekn12 <nabhishek865@gmail.com>
2021-06-13 03:22:31 +0530

    aa

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var a=8;
-var b= 3;
-console.log("a is "+a);
-console.log("b is "+b);
-var c;
-c=a;
-a=b;
-b=c;
-console.log("a is "+a);
-console.log("b is "+b);
+// var a=8;
+// var b= 3;
+// console.log("a is "+a);
+// console.log("b is "+b);
+// var c;
+// c=a;
+// a=b;
+// b=c;
+// console.log("a is "+a);
+// console.log("b is "+b);
+
+var name = "Abhishek";
+console.log(name.length)
+console.log(name.slice(0,4))
