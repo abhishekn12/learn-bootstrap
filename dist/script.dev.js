6873a6d5d2508594e2ba51bf2d4560e139756e82
abhishekn12 <nabhishek865@gmail.com>
2021-06-12 21:59:35 +0530

    js

---
+++ dist/script.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("HELLO");
+console.log("Abhishek");
+var name = "Abhishek";
+console.log(name);
+var yourName = prompt("Your name");
+console.log(yourName);
--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-
-console.log("HELLO");
+var a=8;
+var b= 3;
+console.log("a is "+a);
+console.log("b is "+b);
+var c;
+c=a;
+a=b;
+b=c;
+console.log("a is "+a);
+console.log("b is "+b);
